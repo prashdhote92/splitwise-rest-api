178b776c2aed0f332ad3dbaa92308bea19372343
prashant.dhote@rxglobal.com <prashant.dhote@reedexpo.co.uk>
2023-08-13 21:02:21 +0530

    Update gitignore

--- Splitwise/Dto/GetExpenseDto.cs
+++ Splitwise/Dto/GetExpenseDto.cs
@@ -2,7 +2,7 @@ namespace Splitwise.Dto;
 
 public class GetExpenseDto
 {
+    public string Id { get; set; }
     public User User { get; set; }
     public DateTime Timestamp { get; set; }
-    public string Id { get; set; }
 }
